25ad5ffe7771d888e307ad2fa7ec899eeab5e4b4
Estherdevdiop <146460572+Estherdevdiop@users.noreply.github.com>
2024-12-21 22:53:49 +0000

    cylinder [DONE]

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,20 @@ fn main() {
                 Position::new(0.0, 0.1, 0.0),
                 Color::new(0.0, 1.0, 0.0),
             )),
-            Box::new(Cylinder::new(
-                Position::new(-1.0, 0.0, -1.0),
-                0.2,
-                1.0,
-                Direction::new(1.0, 1.0, -1.0),
-                Color::new(0.8, 0.0, 0.8),
+            // Box::new(Cylinder::new(
+            //     Position::new(-1.0, 0.0, -1.0),
+            //     0.2,
+            //     1.0,
+            //     Direction::new(1.0, 1.0, -1.0),
+            //     Color::new(0.8, 0.0, 0.8),
+            // )),
+        
+          Box::new(Cylinder::new(
+                Position::new(0.8, -0.5, -1.0), 
+                0.2,                             
+                1.0,                            
+                Direction::new(0.0, 1.0, 0.0), 
+                Color::new(0.8, 0.0, 0.8),      
             )),
             Box::new(Sphere::new(
                 Position::new(0.0, 0.0, -1.0),
