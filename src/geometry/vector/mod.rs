5f57984067643c579cdf75d39a161572a511ddb0
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-12-24 13:53:25 +0000

    Random Vector...

--- src/geometry/vector/mod.rs
+++ src/geometry/vector/mod.rs
@@ -1,3 +1,8 @@
+use crate::common::{
+    random_double,
+    random_double_range,
+};
+
 mod mutation;
 mod scalar_ops;
 mod vector_ops;
@@ -11,6 +16,22 @@ pub type Direction = Vector;
 
 impl Vector {
     pub fn new(x: f64, y: f64, z: f64) -> Self { Self(x, y, z) }
+
+    pub fn random() -> Self {
+        Self(
+            random_double(),
+            random_double(),
+            random_double(),
+        )
+    }
+
+    pub fn random_range(min: f64, max: f64) -> Self {
+        Self(
+            random_double_range(min, max),
+            random_double_range(min, max),
+            random_double_range(min, max),
+        )
+    }
 
     pub fn x(&self) -> f64 { self.0 }
 
