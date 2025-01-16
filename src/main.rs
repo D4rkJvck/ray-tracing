d8418f57ddeca3783c8a2bfaa43a1a2de1d5ada8
ndiediop <ndieyed48@gmail.com>
2025-01-16 15:07:01 +0000

    j'ai ajoute pour l'instant trois scenes et les aficher chacun dans un fichier specifique[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D j'ai ajoute pour l'instant trois scenes et les aficher chacun dans un fichier specifique

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,54 @@ fn main() {
             Light::new(
                 Position::new(2.0, 2.0, -1.0),
                 Color::new(1.0, 1.0, 1.0),
+                1.0,
+            ),
+        ],
+        vec![
+            Box::new(Sphere::new(
+                Position::new(0.0, 0.0, -2.0),
+                0.5,
+                Color::new(0.1, 0.0, 0.0),
+            )),
+            Box::new(FlatPlane::new(
+                Position::new(0.0, -0.5, -1.0),
+                Position::new(0.0, 1.0, 0.0),
+                Color::new(0.1, 0.1, 0.0),
+            )),
+        ],
+    );
+    scene1.display("scene1_output.ppm");
+
+    let mut scene2 = Scene::new(
+        Camera::new(Position::default()),
+        vec![
+            Light::new(
+                Position::new(2.0, 2.0, -1.0),
+                Color::new(1.0, 1.0, 1.0),
+                0.5, 
+            ),
+        ],
+        vec![
+            Box::new(FlatPlane::new(
+                Position::new(0.0, -0.5, -1.0), 
+                Position::new(0.0, 1.0, 0.0),   
+                Color::new(0.5, 0.5, 0.5),      
+            )),
+            Box::new(Cube::new(
+                Position::new(-1.5, 0.0, -2.5),
+                1.0,                           
+                Color::new(0.0, 0.2, 0.5),      
+            )),
+        ],
+    );
+    scene2.display("scene2_output.ppm");
+
+    let mut scene3 = Scene::new(
+        Camera::new(Position::default()),
+        vec![
+            Light::new(
+                Position::new(2.0, 2.0, -1.0),
+                Color::new(1.0, 1.0, 1.0),
                 1.0, 
             ),
         ],
@@ -49,53 +97,7 @@ fn main() {
             )),
         ],
     );
-    scene1.display("scene1_output.ppm");
-
-    let mut scene2 = Scene::new(
-        Camera::new(Position::default()),
-        vec![
-            Light::new(
-                Position::new(2.0, 2.0, -1.0),
-                Color::new(1.0, 1.0, 1.0),
-                0.5, 
-            ),
-        ],
-        vec![
-            Box::new(FlatPlane::new(
-                Position::new(0.0, -0.5, -1.0), 
-                Position::new(0.0, 1.0, 0.0),   
-                Color::new(0.5, 0.5, 0.5),      
-            )),
-            Box::new(Cube::new(
-                Position::new(-1.5, 0.0, -2.5),
-                1.0,                           
-                Color::new(0.0, 0.2, 0.5),      
-            )),
-        ],
-    );
-    scene2.display("scene2_output.ppm");
-    
-    let mut scene3 = Scene::new(
-        Camera::new(Position::default()),
-        vec![
-            Light::new(
-                Position::new(2.0, 2.0, -1.0),
-                Color::new(1.0, 1.0, 1.0),
-                1.0,
-            ),
-        ],
-        vec![
-            Box::new(Sphere::new(
-                Position::new(0.0, 0.0, -2.0),
-                0.5,
-                Color::new(0.1, 0.0, 0.0),
-            )),
-            Box::new(FlatPlane::new(
-                Position::new(0.0, -0.5, -1.0),
-                Position::new(0.0, 1.0, 0.0),
-                Color::new(0.1, 0.1, 0.0),
-            )),
-        ],
-    );
     scene3.display("scene3_output.ppm");
+    
+   
 }
